dd91928784fca39432e27c72ad8f850308f0aa52
Dianna Tingg <dtingg@gmail.com>
2019-11-23 16:54:30 -0800

    added letter_freq

---
+++ C10/letter_freq.rb
@@ -0,0 +1,33 @@
+# C10: Given a string, e.g. 'rrSSSqqrrrr' return the letter and count 
+# where a letter consecutively occurs the greatest number of times e.g. 'r4'
+
+# Time: O(n). N is length of string.
+# Space: O(1)
+
+def letter_freq(string)
+  length = string.length
+  i = 0
+  
+  max_letter = ""
+  max_count = 0
+  
+  count = 1
+  
+  while i < length
+    if string[i] == string[i+1]
+      count += 1
+    else
+      if count > max_count
+        max_count = count
+        max_letter = string[i]
+      end
+      count = 1
+    end
+    i += 1
+  end
+  
+  return "#{max_letter}#{max_count}"
+end
+
+p letter_freq("rrSSSqqrrrr") # r4
+p letter_freq("aaabbbccdd") # a3
