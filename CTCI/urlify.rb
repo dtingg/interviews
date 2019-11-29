febd8d86eb9df73452e0b2c855f2745703b148e7
Dianna Tingg <dtingg@gmail.com>
2019-11-28 16:11:45 -0800

    added urlify

---
+++ CTCI/urlify.rb
@@ -0,0 +1,32 @@
+# CTCI: URLify: Write a method to replace all spaces in a string with "%20". 
+# You may assume that the string has sufficient space at the end to hold the additional characters, 
+# and that you are given the "true" length of the string.
+
+# Time: O(n)
+# Space: O(1)
+
+def urlify(string, true_length)  
+  i = true_length - 1 # looking
+  j = -1 # writing
+  
+  while i >= 0
+    if string[i] != " "
+      string[j] = string[i]
+      i -= 1
+      j -= 1
+    else
+      string[j] = "0"
+      j -= 1
+      string[j] = "2"
+      j -= 1
+      string[j] = "%"
+      j -= 1
+      i -= 1
+    end
+  end
+  
+  return string
+end
+
+p urlify("Mr John Smith    ", 13) # "Mr%20John%20Smith"
+p urlify("Happy Ending  ", 12) # "Happy%20Ending"
